4938c59096d7c88e91a25beb5fa450bf2fb8bc26
Junior331 <jarilson.rk@gmail.com>
2025-02-05 00:36:14 -0300

    Fix

--- src/components/organism/Layout/Layout.tsx
+++ src/components/organism/Layout/Layout.tsx
@@ -1,19 +1,32 @@
 "use client";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 import { LayoutProps } from "./@types";
-import { GlobalStyles } from "@/styles/globalStyled";
 import { Snackbar } from "@/components/modules";
+import { GlobalStyles } from "@/styles/globalStyled";
 import * as S from "./LayoutStyled";
 
 const Layout = ({ children }: LayoutProps) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const alreadyReloaded = sessionStorage.getItem("reloaded");
+
+    if (!alreadyReloaded) {
+      sessionStorage.setItem("reloaded", "true");
+      window.location.reload();
+    } else {
+      sessionStorage.removeItem("reloaded");
+    }
+  }, [pathname]);
+
   return (
     <S.LayoutContainer>
       <GlobalStyles />
       <Snackbar />
       <S.LayoutContent>
-        <S.Content>
-          {children}
-        </S.Content>
+        <S.Content>{children}</S.Content>
       </S.LayoutContent>
     </S.LayoutContainer>
   );
