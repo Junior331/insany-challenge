903f0290e9579a8b696a5ce0fcaecae2da3fa489
Junior331 <jarilson.rk@gmail.com>
2025-02-05 03:29:26 -0300

    fix

--- src/components/organism/Sections/Blog/Blog.tsx
+++ src/components/organism/Sections/Blog/Blog.tsx
@@ -25,11 +25,6 @@ export const Blog = () => {
     listAllPosts();
   }, []);
 
-  useEffect(() => {
-    console.log("posts ::", posts);
-    console.log("Boolean(posts.length) ::", Boolean(posts.length));
-  }, [posts]);
-
   return (
     <S.Section id="Blog">
       <S.Header>
@@ -80,35 +75,6 @@ export const Blog = () => {
           },
         }}
       >
-        {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((_i, index) => (
-          <SwiperSlide key={index}>
-            <S.Card onClick={() => router.push(`/project/1`)}>
-              <S.Image src={getImage("avatar").src} alt="Image avatar" />
-
-              <S.ContentCard>
-                <S.HeaderCard>
-                  <S.TitleBlue size={1.6}>Business</S.TitleBlue>
-                  <S.Text size={1.6}>Outubro 2021 </S.Text>
-                </S.HeaderCard>
-                <S.Text>
-                  Sollicitudin a sagittis, risus nisl, fermentum, tincidunt
-                  dolor
-                </S.Text>
-
-                <S.ContainerUser>
-                  <S.ImageAvatar
-                    src={getImage("avatar").src}
-                    alt="Image avatar"
-                  />
-                  <S.ContainerText>
-                    <S.Text>Savannah Nguyen</S.Text>
-                    <S.Text>Autor</S.Text>
-                  </S.ContainerText>
-                </S.ContainerUser>
-              </S.ContentCard>
-            </S.Card>
-          </SwiperSlide>
-        ))} */}
         {posts.map((item) => (
           <SwiperSlide key={item.id}>
             <S.Card onClick={() => router.push(item.link)}>
